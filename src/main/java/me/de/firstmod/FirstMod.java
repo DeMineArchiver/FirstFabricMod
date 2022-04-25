8764a7305946416d679176ed9d447be6b65788f3
DeMineArchiver <minearchiver@gmail.com>
2022-04-25 19:41:54 +0300

    Setup

---
+++ src/main/java/me/de/firstmod/FirstMod.java
@@ -0,0 +1,11 @@
+package me.de.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstMod implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
